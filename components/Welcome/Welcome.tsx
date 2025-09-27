618e08584d742bdfacf5797f6081ca35d4f0ef9e
Nelyubin Mark <mark.nelubin@gmail.com>
2025-09-27 16:16:36 +0300

    commit just4fun

--- components/Welcome/Welcome.tsx
+++ components/Welcome/Welcome.tsx
@@ -5,18 +5,16 @@ export function Welcome() {
   return (
     <>
       <Title className={classes.title} ta="center" mt={100}>
-        Welcome to{' '}
+        Привет,{' '}
         <Text inherit variant="gradient" component="span" gradient={{ from: 'pink', to: 'yellow' }}>
-          Mantine
+          Арина!
         </Text>
       </Title>
       <Text c="dimmed" ta="center" size="lg" maw={580} mx="auto" mt="xl">
-        This starter Next.js project includes a minimal setup for server side rendering, if you want
-        to learn more on Mantine + Next.js integration follow{' '}
-        <Anchor href="https://mantine.dev/guides/next/" size="lg">
-          this guide
+        Здесь будет вся топовая инфа по буддизму (и не только). По всем вопросам обращайся{' '}
+        <Anchor href="https://t.me/Millk04Arinka" size="lg">
+          к моей ассистентке.
         </Anchor>
-        . To get started edit page.tsx file.
       </Text>
     </>
   );
