2e4c0001bb5abd3089fd630265e37d739a05b695
Nelyubin Mark <mark.nelubin@gmail.com>
2025-09-27 20:40:14 +0300

    create magazine structure and added real content

---
+++ app/magazine/[slug]/page.tsx
@@ -0,0 +1,12 @@
+export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
+  const { slug } = await params;
+  const { default: Post } = await import(`@/app/content/articles/${slug}.mdx`);
+
+  return <Post />;
+}
+
+export function generateStaticParams() {
+  return [{ slug: 'four-noble-truths' }];
+}
+
+export const dynamicParams = false;
