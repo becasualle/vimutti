077729061794b64c8778baacd74c945a1803f846
Nelyubin Mark <mark.nelubin@gmail.com>
2025-09-28 18:04:53 +0300

    added og meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,23 @@ export const metadata = {
   title: 'Путь к освобождению',
   description:
     'Обретите эмоциональный покой и свободу благодаря проверенным инструментам психологии, философии и буддизма.',
+  openGraph: {
+    title: 'Путь к освобождению',
+    description:
+      'Обретите эмоциональный покой и свободу благодаря проверенным инструментам психологии, философии и буддизма.',
+    url: 'https://www.vimutti.ru/',
+    siteName: 'Путь к освобождению',
+    images: [],
+    locale: 'ru_RU',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Путь к освобождению',
+    description:
+      'Обретите эмоциональный покой и свободу благодаря проверенным инструментам психологии, философии и буддизма.',
+    images: ['https://www.vimutti.ru/'],
+  },
 };
 
 export default function RootLayout({ children }: { children: any }) {
