f2730d2ab48a866bb78b861f571f7a2e2d41ef68
Nelyubin Mark <mark.nelubin@gmail.com>
2025-11-19 19:33:17 +0300

    slugs generator

---
+++ app/content/utils/slugs-generator.ts
@@ -0,0 +1,29 @@
+import { readdirSync, statSync } from 'fs';
+import path from 'path';
+
+const articlesDirectory = path.join(process.cwd(), 'app/content/articles');
+
+export function getAllSlugs() {
+  return getFiles(articlesDirectory);
+}
+
+function getFiles(dir: string) {
+  const items = readdirSync(dir);
+  const slugs: string[][] = [];
+
+  for (const item of items) {
+    const fullPath = path.join(dir, item);
+    const isDirectory = statSync(fullPath).isDirectory();
+
+    if (isDirectory) {
+      const subSlugs = getFiles(fullPath);
+      slugs.push(...subSlugs);
+    } else if (item.endsWith('.mdx')) {
+      const relativePath = path.relative(articlesDirectory, fullPath);
+      const slug = relativePath.replace(/\.mdx/, '').split(path.sep);
+      slugs.push(slug);
+    }
+  }
+
+  return slugs;
+}
