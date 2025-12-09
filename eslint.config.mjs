2d571dbc5e442bc61f04771763c8dd90c854dbba
Nelyubin Mark <mark.nelubin@gmail.com>
2025-12-09 10:35:38 +0300

    removed redundant mantine mentions

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,9 @@
-import mantine from 'eslint-config-mantine';
 import { defineConfig } from 'eslint/config';
 import tseslint from 'typescript-eslint';
 
 // @ts-check
 export default defineConfig(
   tseslint.configs.recommended,
-  ...mantine,
   { ignores: ['**/*.{mjs,cjs,js,d.ts,d.mts}', '.next'] },
   {
     files: ['**/*.story.tsx'],
