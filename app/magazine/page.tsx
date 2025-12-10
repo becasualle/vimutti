294bfc05c1c860fa9f921700bf8d18b6bbc4b9a5
Nelyubin Mark <mark.nelubin@gmail.com>
2025-12-10 20:30:45 +0300

    moved article cards to separate component

--- app/magazine/page.tsx
+++ app/magazine/page.tsx
@@ -1,55 +1,5 @@
-import {
-  Card,
-  CardAction,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
+import { ArticleList } from '@/components/features/magazine/article-list';
 
 export default function Page() {
-  const cards = [
-    {
-      title: 'Заголовок статьи 1',
-      description: 'Текстовое описание о чем будет статья',
-      action: 'Читать далее',
-      content: 'Какой-то контент',
-      footer: 'Опциональный футер контент',
-    },
-    {
-      title: 'Заголовок статьи 2',
-      description: 'Текстовое описание о чем будет статья',
-      action: 'Читать далее',
-      content: 'Какой-то контент',
-      footer: 'Опциональный футер контент',
-    },
-    {
-      title: 'Заголовок статьи 3',
-      description: 'Текстовое описание о чем будет статья',
-      action: 'Читать далее',
-      content: 'Какой-то контент',
-      footer: 'Опциональный футер контент',
-    },
-  ];
-
-  const cardsTemplate = cards.map((c) => {
-    return (
-      <Card key={c.title}>
-        <CardHeader>
-          <CardTitle>{c.title}</CardTitle>
-          <CardDescription>{c.description}</CardDescription>
-          <CardAction>{c.action}</CardAction>
-        </CardHeader>
-        <CardContent>
-          <p>{c.content}</p>
-        </CardContent>
-        <CardFooter>
-          <p>{c.footer}</p>
-        </CardFooter>
-      </Card>
-    );
-  });
-
-  return cardsTemplate;
+  return <ArticleList />;
 }
